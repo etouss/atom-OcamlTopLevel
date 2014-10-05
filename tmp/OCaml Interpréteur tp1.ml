acb67bee1e4ec660349fd64903e67209db41cfff
Adequin Renaud <adequin.renaud@gmail.com>
2014-10-05 13:11:12 +0200

    utilistation de l'interpreteur Ocaml

---
+++ tmp/OCaml Interpréteur tp1.ml
@@ -0,0 +1,20 @@
+        OCaml version 4.02.0
+
+val carree : int -> int = <fun>
+val perimetre : float -> float = <fun>
+val bis : string -> string = <fun>
+val huit_fois : string -> string = <fun>
+val huit_fois : string -> string = <fun>
+val est_nul : int -> bool = <fun>
+val msg_nul : int -> string = <fun>
+val my_max : 'a -> 'a -> 'a = <fun>
+val max_triple : 'a -> 'a -> 'a -> 'a = <fun>
+val max_quadruple : 'a -> 'a -> 'a -> 'a -> 'a = <fun>
+val sum : int -> int = <fun>
+val fibo : int -> int = <fun>
+val ack : int -> int -> int = <fun>
+val vider : int -> int = <fun>
+val x : int = 2
+- : int = 7
+- : int = 6
+
